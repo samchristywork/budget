70758519d1c33c4355356aa135649729f252cccd
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-08-26 19:29:16 -0500

    feat: Add utility functions
    These functions get user input, and add line items to an existing array
    of line items.

--- main.go
+++ main.go
@@ -33,5 +33,51 @@ func serialize(lineItems []LineItem) {
 	}
 }
 
+func getInput(prompt string) string {
+	fmt.Print(prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	return strings.TrimSpace(text)
+}
+
+func addNewLineItems(lineItems []LineItem, f *os.File) []LineItem {
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		exists := false
+		for _, lineItem := range lineItems {
+			if strings.Contains(line, lineItem.Data) {
+				exists = true
+				continue
+			}
+		}
+		if exists {
+			continue
+		}
+
+		fmt.Println(line)
+		lineItem := LineItem{}
+		lineItem.Category = getInput("Category: ")
+		if lineItem.Category == "" {
+			lineItem.Category = "Ignored"
+			lineItem.Data = line
+			lineItems = append(lineItems, lineItem)
+		} else {
+			lineItem.Date = getInput("Date: ")
+			lineItem.Amount = getInput("Amount: ")
+			lineItem.Data = line
+			lineItems = append(lineItems, lineItem)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file: ", err)
+	}
+
+	return lineItems
+}
+
 func main() {
 }
